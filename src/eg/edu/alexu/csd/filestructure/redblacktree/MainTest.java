5677558486708524fe52e6d204adfbcb7e184a5f
Ahmad Abdallah <ahmed_mosolino@yahoo.com>
2019-05-01 18:14:34 +0200

    almost done

--- src/eg/edu/alexu/csd/filestructure/redblacktree/MainTest.java
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/MainTest.java
@@ -1,30 +1,48 @@
 package eg.edu.alexu.csd.filestructure.redblacktree;
 
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 public class MainTest {
     public static void main(String[] args) {
-        IRedBlackTree<Integer,String> redBlackTree = new RedBlackTree();
-        Integer x = 15;
-        Class c = x.getClass();
+//        IRedBlackTree<Integer,String> redBlackTree = new RedBlackTree();
+//        Integer x = 15;
+//        Class c = x.getClass();
 //        System.out.println(redBlackTree.isEmpty());
 //        redBlackTree.insert(5, "one");
 //        redBlackTree.insert(3, "two");
 //        redBlackTree.insert(1, "three");
 //        redBlackTree.insert(7, "four");
 //        System.out.println(redBlackTree.getRoot().getKey());
-        redBlackTree.insert(5, "5");
-        redBlackTree.insert(2, "2");
-        redBlackTree.insert(7, "7");
-        redBlackTree.insert(4, "4");
-        redBlackTree.insert(12, "12");
-        redBlackTree.insert(1, "1");
-        redBlackTree.insert(8, "8");
-        redBlackTree.insert(20, "20");
-        redBlackTree.insert(10, "10");
-        redBlackTree.insert(9, "9");
-        redBlackTree.insert(16, "16");
-        System.out.println();
+//        redBlackTree.insert(5, "5");
+//        redBlackTree.insert(2, "2");
+//        redBlackTree.insert(7, "7");
+//        redBlackTree.insert(4, "4");
+//        redBlackTree.insert(12, "12");
+//        redBlackTree.insert(1, "1");
+//        redBlackTree.insert(8, "8");
+//        redBlackTree.insert(20, "20");
+//        redBlackTree.insert(10, "10");
+//        redBlackTree.insert(9, "9");
+//        redBlackTree.insert(16, "16");
+        ITreeMap treeMap = new TreeMap();
+        treeMap.put(5, "5");
+        treeMap.put(2, "2");
+        treeMap.put(7, "7");
+        treeMap.put(4, "4");
+        treeMap.put(12, "12");
+        treeMap.put(1, "1");
+        treeMap.put(8, "8");
+        treeMap.put(20, "20");
+        treeMap.put(10, "10");
+        treeMap.put(9, "9");
+        treeMap.put(16, "16");
+        Collection arrayList = treeMap.values();
+        java.util.TreeMap<Integer, String> t = new java.util.TreeMap<Integer, String>();
+        Map.Entry entry = t.firstEntry();
+        System.out.println(treeMap.containsValue("9"));
+
     }
 }
---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/TreeMap.java
@@ -0,0 +1,235 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+import javax.management.RuntimeErrorException;
+import java.util.*;
+
+public class TreeMap<T extends Comparable<T>, V> implements ITreeMap<T, V> {
+
+    private IRedBlackTree<T, V> redBlackTree = new RedBlackTree<>();
+    private int size = 0;
+
+    @Override
+    public Map.Entry<T, V> ceilingEntry(T key) {
+        if (key == null) throw new RuntimeErrorException(new Error());
+        INode<T, V> x = binarySearch(redBlackTree.getRoot(), key);
+        return new AbstractMap.SimpleEntry<>(x.getKey(), x.getValue());
+    }
+
+    @Override
+    public T ceilingKey(T key) {
+        if (key == null) throw new RuntimeErrorException(new Error());
+        return binarySearch(redBlackTree.getRoot(), key).getKey();
+    }
+
+    @Override
+    public void clear() {
+        redBlackTree.clear();
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(T key) {
+        return redBlackTree.contains(key);
+    }
+
+    @Override
+    public boolean containsValue(V value) {
+        if (value == null) throw new RuntimeErrorException(new Error());
+        for (V v : values())
+            if (v.equals(value))
+                return true;
+        return false;
+    }
+
+    @Override
+    public Set<Map.Entry<T, V>> entrySet() {
+        Set<Map.Entry<T, V>> set = new LinkedHashSet<>();
+        INode<T, V> node = redBlackTree.getRoot();
+        Stack<INode<T, V>> stack = new Stack<>();
+        arrayList = new ArrayList<>();
+        inline(redBlackTree.getRoot(), null);
+        for (int i = 0; i < arrayList.size(); i++)
+            set.add(arrayList.get(i));
+        return set;
+    }
+
+    private ArrayList<Map.Entry<T, V>> arrayList = new ArrayList<>();
+
+    private void inline(INode<T, V> node, T key) {
+        if (node.isNull()) return;
+        inline(node.getLeftChild(), key);
+        // TODO: 5/1/2019 do something
+        if (key == null)
+//            arrayList.add(node);
+            arrayList.add(new AbstractMap.SimpleEntry<>(node.getKey(), node.getValue()));
+        else if (node.getKey().compareTo(key) <= 0)
+//            arrayList.add(node);
+            arrayList.add(new AbstractMap.SimpleEntry<>(node.getKey(), node.getValue()));
+        inline(node.getRightChild(), key);
+    }
+
+    @Override
+    public Map.Entry<T, V> firstEntry() {
+        if (size == 0)
+            return null;
+        INode<T, V> node = redBlackTree.getRoot();
+        if (node.getLeftChild() != null)
+            while (!node.getLeftChild().isNull())
+                node = node.getLeftChild();
+        return new AbstractMap.SimpleEntry<>(node.getKey(), node.getValue());
+    }
+
+    @Override
+    public T firstKey() {
+        Map.Entry entry = firstEntry();
+        if (entry != null)
+            return firstEntry().getKey();
+        else return null;
+    }
+
+    @Override
+    public Map.Entry<T, V> floorEntry(T key) {
+        V temp = redBlackTree.search(key);
+        if (temp == null) {
+            arrayList = new ArrayList<>();
+            inline(redBlackTree.getRoot(), key);
+            return arrayList.get(arrayList.size() - 1);
+        } else return new AbstractMap.SimpleEntry<>(key, temp);
+    }
+
+    @Override
+    public T floorKey(T key) {
+        return floorEntry(key).getKey();
+    }
+
+    @Override
+    public V get(T key) {
+        if (key == null) throw new RuntimeErrorException(new Error());
+        return redBlackTree.search(key);
+    }
+
+    @Override
+    public ArrayList<Map.Entry<T, V>> headMap(T toKey) {
+        if (toKey == null) throw new RuntimeErrorException(new Error());
+        arrayList = new ArrayList<>();
+        inline(redBlackTree.getRoot(), toKey);
+        if (arrayList.get(arrayList.size() - 1).getKey().compareTo(toKey) == 0)
+            arrayList.remove(arrayList.size() - 1);
+        return arrayList;
+    }
+
+    @Override
+    public ArrayList<Map.Entry<T, V>> headMap(T toKey, boolean inclusive) {
+        if (toKey == null) throw new RuntimeErrorException(new Error());
+        arrayList = new ArrayList<>();
+        inline(redBlackTree.getRoot(), toKey);
+        return inclusive ? arrayList : headMap(toKey);
+    }
+
+    @Override
+    public Set<T> keySet() {
+        Set<T> keys = new LinkedHashSet<>();
+        arrayList = new ArrayList<>();
+        inline(redBlackTree.getRoot(), null);
+        for (int i = 0; i < arrayList.size(); i++) {
+            keys.add(arrayList.get(i).getKey());
+        }
+        return keys;
+    }
+
+    @Override
+    public Map.Entry<T, V> lastEntry() {
+        if (size == 0)
+            return null;
+        INode<T, V> node = redBlackTree.getRoot();
+        if (node.getRightChild() != null)
+            while (!node.getRightChild().isNull())
+                node = node.getRightChild();
+        return new AbstractMap.SimpleEntry<>(node.getKey(), node.getValue());
+    }
+
+    @Override
+    public T lastKey() {
+        Map.Entry<T, V> entry = lastEntry();
+        if (entry != null)
+            return lastEntry().getKey();
+        else return null;
+    }
+
+    @Override
+    public Map.Entry<T, V> pollFirstEntry() {
+        Map.Entry<T, V> entry = firstEntry();
+        if (entry != null)
+            remove(entry.getKey());
+        return entry;
+    }
+
+    @Override
+    public Map.Entry<T, V> pollLastEntry() {
+        Map.Entry<T, V> entry = lastEntry();
+        if (entry != null)
+            remove(entry.getKey());
+        return entry;
+    }
+
+    @Override
+    public void put(T key, V value) {
+        if (key == null || value == null) throw new RuntimeErrorException(new Error());
+        if(!redBlackTree.contains(key))
+            size++;
+        redBlackTree.insert(key, value);
+    }
+
+    @Override
+    public void putAll(Map<T, V> map) {
+        if (map == null) throw new RuntimeErrorException(new Error());
+        for (Map.Entry<T, V> entry : map.entrySet()) {
+            redBlackTree.insert(entry.getKey(), entry.getValue());
+            size++;
+        }
+    }
+
+    @Override
+    public boolean remove(T key) {
+        if (key == null) throw new RuntimeErrorException(new Error());
+        boolean ans = redBlackTree.delete(key);
+        if (ans) size--;
+        return ans;
+
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public Collection<V> values() {
+        arrayList = new ArrayList<>();
+        inline(redBlackTree.getRoot(), null);
+        ArrayList<V> ans = new ArrayList<>();
+        for (Map.Entry<T, V> entry : arrayList) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+
+    private Stack<INode<T, V>> stack = new Stack<>();
+
+    private INode<T, V> binarySearch(INode<T, V> node, T key) {
+        stack.push(node);
+        if (node.getKey() == null)
+            if (node != redBlackTree.getRoot() && node == redBlackTree.getRoot().getParent()) {
+                stack.pop();
+                INode x = stack.pop();
+                stack = new Stack<>();
+                return x;
+            } else throw new RuntimeErrorException(new Error());
+        if (node.getKey().compareTo(key) == 0)
+            return node;
+        if (node.getKey().compareTo(key) > 0)
+            return binarySearch(node.getLeftChild(), key);
+        else
+            return binarySearch(node.getRightChild(), key);
+    }
+}
